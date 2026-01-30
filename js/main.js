a6d3e80e5ddd6fc5e1c47398b66bec95aa9cb341
JenniferPaolaVerdeEspitia <jennif3rpaol402@gmail.com>
2026-01-29 22:55:52 -0600

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+canvas.width = 400;
+canvas.height = 300;
+
+ctx.fillStyle = "purple";
+ctx.fillRect(10, 10, 100, 100);
